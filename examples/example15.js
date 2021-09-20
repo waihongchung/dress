957755111961cfa244149ca3cfbe97f3ac9791f0
waihongchung <78774982+waihongchung@users.noreply.github.com>
2021-09-20 17:50:50 -0400

    Delete examples directory

--- examples/example15.js
+++
@@ -1,9 +0,0 @@
-function processJSON(subjects) {
-
-
-    DRESS.print(DRESS.histograms(subjects, ['Age', 'BMI', 'Disease Duration', 'HA1C'], ['Gender', 'Race', 'Smoking', 'Alcohol', 'Etiology']));
-
-
-}
-
-DRESS.local('data.json', processJSON);
